fcca2421e641b7a13b72ebcf9ce53d3cca24626c
tejes <tmank14319@gmail.com>
2023-01-13 10:17:22 +0530

    test

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,14 @@ fn main() {
 
                     let  mut buf = [0;128];
                     let i = stream.read(&mut buf).expect("error encodoing to string");
-                    let st = String::from_utf8(buf[..i].to_vec()).expect("asas");
-                    println!("{}",st) ;
-                 // if stream_string == "PING" || stream_string == "ping"{
-                 //     println!("{}",simple_string_encoder("PONG".to_string()));
-                 //     write!(stream,"{}",simple_string_encoder("PONG".to_string())).expect("error writeing to stream");
+                    let stream_string = String::from_utf8(buf[..i].to_vec()).expect("asas");
+                    let vec_of_command = convert_to_vec_of_msg(stream_string);
+
+                if vec_of_command[0].1 == "PING" || vec_of_command[0].1 == "ping"{
+                     println!("{}",simple_string_encoder("PONG".to_string()));
+                     write!(stream,"{}",simple_string_encoder("PONG".to_string())).expect("error writeing to stream");
                      
-                 // }
+                 }
              }
              Err(e) => {
                  println!("error: {}", e);
@@ -33,4 +34,26 @@ fn main() {
 fn simple_string_encoder(data:String)->String{
        format!("+{}\r\n",data) 
 
+}
+
+
+fn convert_to_vec_of_msg(s:String)->Vec<(i32, String)>{
+   let mut vec_of_commands:Vec<(i32,String)> = Vec::new();
+   for i in s.lines(){
+        let mut t:(i32,String)  = (0,"".to_string());
+        if i.contains("*"){
+
+        }
+        if i.contains("$"){
+        t.0 = i[1..].parse::<i32>().expect("error while parsing to i32");
+         
+        }else{
+            t.1 =i.to_string();
+
+        }
+
+            vec_of_commands.push(t);
+
+   }
+   vec_of_commands
 }
