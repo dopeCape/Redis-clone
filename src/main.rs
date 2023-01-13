cc9ec4aa781943c6f636897f154ea9be13eb6fcb
tejes <tmank14319@gmail.com>
2023-01-13 11:03:07 +0530

    test

--- src/main.rs
+++ src/main.rs
@@ -26,9 +26,19 @@ fn simple_string_encoder(data: String) -> String {
 
 fn responder(mut stream: TcpStream) {
     let mut buf = [0; 128];
-     stream.read(&mut buf).expect("error encodoing to string");
+     let x =stream.read(&mut buf).expect("error encodoing to string");
+
+     let mut looper  =0;
+    let stream_stringz = String::from_utf8(buf[..x].to_vec()).expect("asas");
+    for l in stream_stringz.lines(){
+        if l.contains("*"){
+
+            looper = l[1..].parse::<i32>().expect("error while parsing to i32");
+
+        }
+    }
      println!("{}",buf[1]);
-    for _ in 0..(buf[1] as usize){
+    for _ in 0..looper{
      let i = stream.read(&mut buf).expect("error encodoing to string");
     let stream_string = String::from_utf8(buf[..i].to_vec()).expect("asas");
     let vec_of_command = convert_to_vec_of_msg(stream_string);
