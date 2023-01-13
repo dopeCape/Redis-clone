f9e3ce6fbce089383bb251f94d566e08627e48b2
tejes <tmank14319@gmail.com>
2023-01-13 09:37:38 +0530

    test

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{net::TcpListener, io::Read,io::Write};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+
 
     // Uncomment this block to pass the first stage   
      let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
@@ -12,10 +12,13 @@ fn main() {
          match stream {
              Ok(mut stream) => {
                  let mut stream_string:String = String::new();
+                println!("connectd to someone");
+
                  stream.read_to_string(&mut stream_string).expect("error encodoing to string");
-        
+                    println!("{}",stream_string) ;
                  if stream_string == "PING" || stream_string == "ping"{
                      write!(stream,"{}",simple_string_encoder("PONG".to_string())).expect("error writeing to stream");
+                     
                  }
              }
              Err(e) => {
