9d14e6360cd38d029949c8b4a33c6c416e6b7998
tejes <tmank14319@gmail.com>
2023-01-15 00:54:09 +0530

    test

--- src/executor.rs
+++ src/executor.rs
@@ -2,14 +2,14 @@
 pub struct Command{
 
     pub ty:Option<String>,
-    pub command:Option<String>,
-
+    pub command:Vec<Option<String>>,
+    
 }
 
 
 impl Command{
        pub fn new(ty:Option<String>,command:Option<String>)->Command{
-           Command {  ty,command }
+           Command {  ty,command:Vec::new() }
         
 
        } 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // Uncomment this block to pass the cstage
-use std::{ net::{ TcpListener, TcpStream }, io::Read, io::Write };
+use std::{ net::{ TcpListener, TcpStream }, io::Read, io::Write, collections::HashMap };
 
 use threds::ThreadPool;
 mod threds;
@@ -40,38 +40,97 @@ fn responder(stream: &mut TcpStream) {
 
     println!("{:?}", vec_of_commands);
     for tup in vec_of_commands.iter() {
-        if tup.command == Some("PING".to_string()) || tup.command == Some("ping".to_string()) {
+        if tup.command[0] == Some("PING".to_string()) || tup.command[0] == Some("ping".to_string()) {
             println!("{}", simple_string_encoder(&"PONG".to_string()));
 
             write!(stream, "{}", simple_string_encoder(&"PONG".to_string())).expect(
                 "error writeing to stream"
             );
         } else if tup.ty == Some("print".to_string()) {
-            match &tup.command {
-                Some(x) => {
-                    write!(stream, "{}", simple_string_encoder(&x)).expect("erooorrrr");
-                }
-                None => {
-                    println!("some thing weird");
+            for i in &tup.command{
+
+
+
+                match i {
+                    Some(x) => {
+                        write!(stream, "{}", simple_string_encoder(&x)).expect("erooorrrr");
+                    }
+                    None => {
+                        println!("some thing weird");
+                    }
                 }
             }
-        }
+        }    else if tup.ty == Some("SET".to_string()){
+                   
+
+                        write!(stream, "{}", get_set_cahcer(tup.ty.to_owned().unwrap(), &tup.command) ).expect("erooorrrr");
+
+            }
+        else if tup.ty == Some("GET".to_string()){
+                   
+
+                        write!(stream, "{}", get_set_cahcer(tup.ty.to_owned().unwrap(), &tup.command) ).expect("erooorrrr");
+
+            }
+
     }
+}
+
+fn get_set_cahcer(method:String,commands:&Vec<Option<String>>)->String{
+
+
+let mut store:HashMap<String,String> = std::collections::HashMap::new();
+if method =="SET"    {
+    let key  = &commands[0].to_owned().unwrap();
+    let value = &commands[1].to_owned().unwrap();
+
+
+        let res = store.insert(key.to_string(), value.to_string());
+
+        if res ==None{
+    
+               return      simple_string_encoder(&"Ok".to_string());
+
+        }else{
+        return simple_string_encoder(&res.unwrap().to_string());
+
+
+        }
+
+
+}else{
+    let key  = &commands[0].to_owned().unwrap();
+        let res = store.get(key);
+ if res ==None{
+    
+               return      simple_string_encoder(&"nil".to_string());
+
+        }else{
+        return simple_string_encoder(&res.unwrap().to_string());
+
+
+        }
+
+
+}
+
+
 }
 fn convert_to_vec_of_msg(s: String, vec_of_commands: &mut Vec<executor::Command>) {
     let mut count = 0;
 
-    let t: executor::Command = executor::Command { ty: None, command: None };
+    let mut t: executor::Command = executor::Command { ty: None, command: Vec::new() };
+    t.command.push(None);
     vec_of_commands.push(t);
-    println!("{}",s);
+    {}
     for i in s.lines() {
-        let t: executor::Command = executor::Command { ty: None, command: None };
+        let mut t: executor::Command = executor::Command { ty: None, command: Vec::new() };
 
-                    println!("{:?}",count);
+    t.command.push(None);
         if i.contains("*") {
         } else if i.contains("$") {
         } else {
-            if vec_of_commands[count].ty != None && vec_of_commands[count].command != None {
+            if vec_of_commands[count].ty != None && vec_of_commands[count].command[0]!= None {
                 vec_of_commands.push(t);
                 count += 1;
             }
@@ -80,22 +139,23 @@ fn convert_to_vec_of_msg(s: String, vec_of_commands: &mut Vec<executor::Command>
                 if i.contains("PING") || i.contains("ping") {
                     vec_of_commands[count].ty = Some("print".to_string());
 
-                    vec_of_commands[count].command = Some("PING".to_string());
-                    println!("{:?}",count);
+                    vec_of_commands[count].command.push( Some("PING".to_string())) ;
 
                     continue;
                }
                 if i.contains("ECHO") || i.contains("echo") {
                     vec_of_commands[count].ty = Some("print".to_string());
-                    println!("{:?}",vec_of_commands[count]);
                     continue;
-                } }if vec_of_commands[count].ty != None {
+                }if i.contains("GET") || i.contains("get"){
+                    vec_of_commands[count].ty = Some("GET".to_string());
+
+
+                }}if vec_of_commands[count].ty != None {
 
                     
-                        vec_of_commands[count].command = Some(i.to_string());
+                    vec_of_commands[count].command.push( Some(i.to_string())) ;
+                
                     
-                    println!("{:?}",count);
-                    println!("{:?}",vec_of_commands[count].command );
                 
             }
         }
