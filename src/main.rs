a850f102865c15024c746a7df9d47cbf3e35a484
tejes <tmank14319@gmail.com>
2023-01-14 22:38:17 +0530

    test

--- src/main.rs
+++ src/main.rs
@@ -104,9 +104,11 @@ fn convert_to_vec_of_msg(s: String,vec_of_commands: &mut Vec<executor::Command>)
             if vec_of_commands[count].ty!=None && vec_of_commands[count].command != Some("PING".to_string()){
                 
      vec_of_commands[count].command = Some(i.to_string());
-    println!("{:?}",vec_of_commands[count].command ); 
+    // println!("{:?}",vec_of_commands[count].command ); 
             count += 1;
-            }
+        let t  :executor::Command  = executor::Command { ty: None, command:  None};
+           vec_of_commands.push(t);
+             }
             }
 
 
