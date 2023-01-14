628460a6c29da757367ae3c94bff89d5a5f1f892
tejes <tmank14319@gmail.com>
2023-01-14 23:03:47 +0530

    test

--- src/main.rs
+++ src/main.rs
@@ -83,7 +83,7 @@ fn convert_to_vec_of_msg(s: String, vec_of_commands: &mut Vec<executor::Command>
                }
                 if i.contains("ECHO") || i.contains("echo") {
                     vec_of_commands[count].ty = Some("print".to_string());
-                } else {
+                } if vec_of_commands[count].ty != None {
 
                         vec_of_commands[count].command = Some(i.to_string());
                     
