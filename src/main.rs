eab88e53aa1c1604555219879cc0bdfbcec4f613
tejes <tmank14319@gmail.com>
2023-01-15 01:21:28 +0530

    test

--- src/main.rs
+++ src/main.rs
@@ -40,17 +40,17 @@ fn responder(stream: &mut TcpStream) {
 
     println!("{:?}", vec_of_commands);
     for tup in vec_of_commands.iter() {
-        if tup.command[0] == Some("PING".to_string()) || tup.command[0] == Some("ping".to_string()) {
+        if
+            tup.command[0] == Some("PING".to_string()) ||
+            tup.command[0] == Some("ping".to_string())
+        {
             println!("{}", simple_string_encoder(&"PONG".to_string()));
 
             write!(stream, "{}", simple_string_encoder(&"PONG".to_string())).expect(
                 "error writeing to stream"
             );
         } else if tup.ty == Some("print".to_string()) {
-            for i in &tup.command{
-
-
-
+            for i in &tup.command {
                 match i {
                     Some(x) => {
                         write!(stream, "{}", simple_string_encoder(&x)).expect("erooorrrr");
@@ -60,61 +60,40 @@ fn responder(stream: &mut TcpStream) {
                     }
                 }
             }
-        }    else if tup.ty == Some("set".to_string()){
-                   
-
-                        write!(stream, "{}", get_set_cahcer(tup.ty.to_owned().unwrap(), &tup.command) ).expect("erooorrrr");
-
-            }
-        else if tup.ty == Some("get".to_string()){
-                   
-
-                        write!(stream, "{}", get_set_cahcer(tup.ty.to_owned().unwrap(), &tup.command) ).expect("erooorrrr");
-
-            }
-
+        } else if tup.ty == Some("set".to_string()) {
+            write!(stream, "{}", get_set_cahcer(tup.ty.to_owned().unwrap(), &tup.command)).expect(
+                "erooorrrr"
+            );
+        } else if tup.ty == Some("get".to_string()) {
+            write!(stream, "{}", get_set_cahcer(tup.ty.to_owned().unwrap(), &tup.command)).expect(
+                "erooorrrr"
+            );
+        }
     }
 }
 
-fn get_set_cahcer(method:String,commands:&Vec<Option<String>>)->String{
-
-
-let mut store:HashMap<String,String> = std::collections::HashMap::new();
-if method =="set"    {
-    let key  = &commands[0].to_owned().unwrap();
-    let value = &commands[1].to_owned().unwrap();
-
+fn get_set_cahcer(method: String, commands: &Vec<Option<String>>) -> String {
+    let mut store: HashMap<String, String> = std::collections::HashMap::new();
+    if method == "set" {
+        let key = &commands[0].to_owned().unwrap();
+        let value = &commands[1].to_owned().unwrap();
 
         let res = store.insert(key.to_string(), value.to_string());
 
-        if res ==None{
-    
-               return      simple_string_encoder(&"Ok".to_string());
-
-        }else{
-        return simple_string_encoder(&res.unwrap().to_string());
-
-
+        if res == None {
+            return simple_string_encoder(&"Ok".to_string());
+        } else {
+            return simple_string_encoder(&res.unwrap().to_string());
         }
-
-
-}else{
-    let key  = &commands[0].to_owned().unwrap();
+    } else {
+        let key = &commands[0].to_owned().unwrap();
         let res = store.get(key);
- if res ==None{
-    
-               return      simple_string_encoder(&"nil".to_string());
-
-        }else{
-        return simple_string_encoder(&res.unwrap().to_string());
-
-
+        if res == None {
+            return simple_string_encoder(&"nil".to_string());
+        } else {
+            return simple_string_encoder(&res.unwrap().to_string());
         }
-
-
-}
-
-
+    }
 }
 fn convert_to_vec_of_msg(s: String, vec_of_commands: &mut Vec<executor::Command>) {
     let mut count = 0;
@@ -123,17 +102,15 @@ fn convert_to_vec_of_msg(s: String, vec_of_commands: &mut Vec<executor::Command>
     t.command.push(None);
     vec_of_commands.push(t);
 
-    println!("{}",s);
+    println!("{}", s);
     for i in s.lines() {
         let mut t: executor::Command = executor::Command { ty: None, command: Vec::new() };
 
-    t.command.push(None);
+        t.command.push(None);
         if i.contains("*") {
         } else if i.contains("$") {
         } else {
-            if vec_of_commands[count].ty != None && vec_of_commands[count].command[0]!= None {
-                vec_of_commands.push(t);
-                
+            if vec_of_commands[count].ty != None && vec_of_commands[count].command[0] != None {
             }
 
             if vec_of_commands[count].ty == None {
@@ -141,37 +118,35 @@ fn convert_to_vec_of_msg(s: String, vec_of_commands: &mut Vec<executor::Command>
                     vec_of_commands[count].ty = Some("print".to_string());
 
                     vec_of_commands[count].command.pop();
-                    vec_of_commands[count].command.push( Some("PING".to_string())) ;
+                    vec_of_commands[count].command.push(Some("PING".to_string()));
 
                     continue;
-               }
+                }
                 if i.contains("ECHO") || i.contains("echo") {
                     vec_of_commands[count].ty = Some("print".to_string());
                     continue;
-                }if i.contains("GET") || i.contains("get"){
-                    vec_of_commands[count].ty = Some("get".to_string());
-
-
-                }if i.contains("SET") || i.contains("set"){
-
-                    vec_of_commands[count].ty = Some("set".to_string());
-
-                    
                 }
-
-            }if vec_of_commands[count].ty != None  && Some(i.to_string()) != vec_of_commands[count].ty{
-
-                     vec_of_commands[count].command.pop();
-                    vec_of_commands[count].command.push( Some(i.to_string())) ;
-   if Some("print".to_string()) == vec_of_commands[count].ty{
-                        count+=1;
-                     }else if Some("get".to_string()) == vec_of_commands[count].ty ||Some("set".to_string()) == vec_of_commands[count].ty{}
-
-
-
-                                  
-                    
-                
+                if i.contains("GET") || i.contains("get") {
+                    vec_of_commands[count].ty = Some("get".to_string());
+                }
+                if i.contains("SET") || i.contains("set") {
+                    vec_of_commands[count].ty = Some("set".to_string());
+                }
+            }
+            if
+                vec_of_commands[count].ty != None &&
+                Some(i.to_string()) != vec_of_commands[count].ty
+            {
+                vec_of_commands[count].command.pop();
+                vec_of_commands[count].command.push(Some(i.to_string()));
+                if Some("print".to_string()) == vec_of_commands[count].ty {
+                vec_of_commands.push(t);
+                    count += 1;
+                } else if
+                    Some("get".to_string()) == vec_of_commands[count].ty ||
+                    Some("set".to_string()) == vec_of_commands[count].ty
+                {
+                }
             }
         }
     }
