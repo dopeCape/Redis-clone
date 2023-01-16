264ce58cbd878d8b482f98bd554f75acecdf972f
tejes <tmank14319@gmail.com>
2023-01-16 10:32:12 +0530

    test

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,28 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
-use std::{ net::{ TcpListener, TcpStream }, io::Read, io::Write, collections::HashMap, fs::write, time::{SystemTime, UNIX_EPOCH} };
+use std::{
+    net::{ TcpListener, TcpStream },
+    io::Read,
+    io::Write,
+    collections::HashMap,
+    fs::write,
+    time::{ SystemTime, UNIX_EPOCH },
+};
 
 use threds::ThreadPool;
 mod threds;
 mod executor;
-use std::sync::{Mutex,Arc};
+use std::sync::{ Mutex, Arc };
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
 
     // Uncomment this block to pass the first stage
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    let  store: Arc<Mutex<HashMap<String, (String,u64)>>> = Arc::new(Mutex::new(std::collections::HashMap::new()));
+    let store: Arc<Mutex<HashMap<String, (String, u64)>>> = Arc::new(
+        Mutex::new(std::collections::HashMap::new())
+    );
     let th = ThreadPool::new(5);
     for stream in listener.incoming() {
         let store = Arc::clone(&store);
@@ -22,7 +31,7 @@ fn main() {
             Ok(mut stream) => {
                 th.execute(move || {
                     loop {
-                        responder(&mut stream,   store.clone());
+                        responder(&mut stream, store.clone());
                     }
                 });
             }
@@ -35,17 +44,15 @@ fn main() {
 }
 
 fn simple_string_encoder(data: &String) -> String {
-    
-    if data.contains("$"){
+    if data.contains("$") {
         return data.to_string();
-
     }
-   let res = format!("+{}\r\n", data);
-       println!("{}",res);
-   res
+    let res = format!("+{}\r\n", data);
+    println!("{}", res);
+    res
 }
 
-fn responder(stream: &mut TcpStream, mut  store: Arc<Mutex<HashMap<String,(String,u64)>>>) {
+fn responder(stream: &mut TcpStream, mut store: Arc<Mutex<HashMap<String, (String, u64)>>>) {
     let mut buf = [0; 128];
     let mut vec_of_commands: Vec<executor::Command> = Vec::new();
     let i = stream.read(&mut buf).expect("error encodoing to string");
@@ -75,102 +82,83 @@ fn responder(stream: &mut TcpStream, mut  store: Arc<Mutex<HashMap<String,(Strin
                 }
             }
         } else if tup.ty == Some("set".to_string()) {
-                write!(stream, "{}", get_set_cahcer(tup.ty.to_owned().unwrap(), &tup.command, &mut store)).expect(
-                    "erooorrrr"
-                    );
-            
-
-                
-
-            
-
+            write!(
+                stream,
+                "{}",
+                get_set_cahcer(tup.ty.to_owned().unwrap(), &tup.command, &mut store)
+            ).expect("erooorrrr");
         } else if tup.ty == Some("get".to_string()) {
-           write!(stream, "{}", get_set_cahcer(tup.ty.to_owned().unwrap(), &tup.command, &mut store)).expect(
-                "erooorrrr"
-            );
+            write!(
+                stream,
+                "{}",
+                get_set_cahcer(tup.ty.to_owned().unwrap(), &tup.command, &mut store)
+            ).expect("erooorrrr");
         }
     }
 }
 
-fn get_set_cahcer(method: String, commands: &Vec<Option<String>>,store:&mut Arc<Mutex<HashMap<String,(String,u64)>>>) -> String {
-
+fn get_set_cahcer(
+    method: String,
+    commands: &Vec<Option<String>>,
+    store: &mut Arc<Mutex<HashMap<String, (String, u64)>>>
+) -> String {
     if method == "set" {
-        if commands.len() == 3{
+        if commands.len() == 3 {
+            let key = &commands[0].to_owned().unwrap();
+            let value = &commands[1].to_owned().unwrap();
 
- let key = &commands[0].to_owned().unwrap();
-        let value = &commands[1].to_owned().unwrap();
+            let res = store.lock().unwrap().insert(key.to_string(), (value.to_string(), 0));
 
-        let res = store.lock().unwrap().insert(key.to_string(), (value.to_string(),0));
-
-        // println!("{:?}",store);
-        if res == None {
-            return simple_string_encoder(&"OK".to_string());
+            // println!("{:?}",store);
+            if res == None {
+                return simple_string_encoder(&"OK".to_string());
+            } else {
+                return simple_string_encoder(&res.unwrap().0.to_string());
+            }
         } else {
-            return simple_string_encoder(&res.unwrap().0.to_string());
-        }
-        }
-        else  {
-            let mut since_the_epoch = SystemTime::now().duration_since(UNIX_EPOCH).unwrap() ;
-            let x = since_the_epoch.as_secs() * 1000 +
-            since_the_epoch.subsec_nanos() as u64 / 1_000_000+
-             commands[3].to_owned().unwrap().parse::<u64>().unwrap();
-             let val  = commands[1].to_owned().unwrap()  ;
-             let res =  store.lock().unwrap().insert(commands[0].to_owned().unwrap(), (val,x));
-             println!("{:?}",store.lock().unwrap());
-                 
-            return  simple_string_encoder(&"OK".to_string());
+            let mut since_the_epoch = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+            let x =
+                since_the_epoch.as_secs() * 1000 +
+                (since_the_epoch.subsec_nanos() as u64) / 1_000_000 +
+                commands[3].to_owned().unwrap().parse::<u64>().unwrap();
+            let val = commands[1].to_owned().unwrap();
+            let res = store.lock().unwrap().insert(commands[0].to_owned().unwrap(), (val, x));
+            println!("{:?}", store.lock().unwrap());
 
+            return simple_string_encoder(&"OK".to_string());
         }
-        }
-        else {
+    } else {
         let key = &commands[0].to_owned().unwrap();
-        let res = store.lock().unwrap();
-        let res = res.get(key);
-            let mut since_the_epoch = SystemTime::now().duration_since(UNIX_EPOCH).unwrap() ;
-            let x = since_the_epoch.as_secs() * 1000 +
-            since_the_epoch.subsec_nanos() as u64 / 1_000_000;
-        println!("{:?} && {:?}",res,x);
+        let  mut store_unlocked = store.lock().unwrap();
+        
+        let res =store_unlocked.get(key);
+        let mut since_the_epoch = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+        let x =
+            since_the_epoch.as_secs() * 1000 + (since_the_epoch.subsec_nanos() as u64) / 1_000_000;
+        println!("{:?} && {:?}", res, x);
         if res == None {
-               println!("{:?}",res) ;
             return simple_string_encoder(&"nil".to_string());
         } else {
-            if res.unwrap().1 == 0{
+            if res.unwrap().1 == 0 {
+                return simple_string_encoder(&res.unwrap().0.to_string());
+            } else {
+                if res.unwrap().1 > x {
+                    return simple_string_encoder(&res.unwrap().0.to_string());
+                } else {
+                    let y = store_unlocked.remove(key);
+                    println!("{:?}", y);
+                    if y != None {
+                        println!("{:?}", y);
 
-               println!("{:?}",res) ;
-
-            return simple_string_encoder(&res.unwrap().0.to_string());
-
-            }else{
-            if res.unwrap().1 > x{
-            return simple_string_encoder(&res.unwrap().0.to_string());
-
-
-            }else{
-            let y = store.lock().unwrap().remove(key);
-               println!("{:?}",y) ;
-            if y != None{
-
-               println!("{:?}",y) ;
-
-            return "$-1\r\n".to_string();
-
-            }else {
-                return "some shiity as eeroro".to_string();
+                        return "$-1\r\n".to_string();
+                    } else {
+                        return "some shiity as eeroro".to_string();
+                    }
+                }
             }
-        
-
-            }
-
-
-            }
-
         }
     }
 }
-
-
-
-
 
 fn convert_to_vec_of_msg(s: String, vec_of_commands: &mut Vec<executor::Command>) {
     let mut count = 0;
@@ -217,14 +205,13 @@ fn convert_to_vec_of_msg(s: String, vec_of_commands: &mut Vec<executor::Command>
                 vec_of_commands[count].command.pop();
                 vec_of_commands[count].command.push(Some(i.to_string()));
                 if Some("print".to_string()) == vec_of_commands[count].ty {
-                vec_of_commands.push(t);
+                    vec_of_commands.push(t);
                     count += 1;
                 } else if
                     Some("get".to_string()) == vec_of_commands[count].ty ||
                     Some("set".to_string()) == vec_of_commands[count].ty
                 {
-                vec_of_commands[count].command.push(None);
-
+                    vec_of_commands[count].command.push(None);
                 }
             }
         }
