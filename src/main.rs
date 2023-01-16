0f53b66ce55eece2bfabf8bf586263cd71822197
tejes <tmank14319@gmail.com>
2023-01-16 09:49:26 +0530

    test

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
-use std::{ net::{ TcpListener, TcpStream }, io::Read, io::Write, collections::HashMap };
+use std::{ net::{ TcpListener, TcpStream }, io::Read, io::Write, collections::HashMap, fs::write, time::{SystemTime, UNIX_EPOCH} };
 
 use threds::ThreadPool;
 mod threds;
@@ -14,7 +14,7 @@ fn main() {
 
     // Uncomment this block to pass the first stage
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    let  store: Arc<Mutex<HashMap<String, String>>> = Arc::new(Mutex::new(std::collections::HashMap::new()));
+    let  store: Arc<Mutex<HashMap<String, (String,u64)>>> = Arc::new(Mutex::new(std::collections::HashMap::new()));
     let th = ThreadPool::new(5);
     for stream in listener.incoming() {
         let store = Arc::clone(&store);
@@ -41,7 +41,7 @@ fn simple_string_encoder(data: &String) -> String {
    res
 }
 
-fn responder(stream: &mut TcpStream, mut  store: Arc<Mutex<HashMap<String,String>>>) {
+fn responder(stream: &mut TcpStream, mut  store: Arc<Mutex<HashMap<String,(String,u64)>>>) {
     let mut buf = [0; 128];
     let mut vec_of_commands: Vec<executor::Command> = Vec::new();
     let i = stream.read(&mut buf).expect("error encodoing to string");
@@ -71,9 +71,15 @@ fn responder(stream: &mut TcpStream, mut  store: Arc<Mutex<HashMap<String,String
                 }
             }
         } else if tup.ty == Some("set".to_string()) {
-            write!(stream, "{}", get_set_cahcer(tup.ty.to_owned().unwrap(), &tup.command, &mut store)).expect(
-                "erooorrrr"
-            );
+                write!(stream, "{}", get_set_cahcer(tup.ty.to_owned().unwrap(), &tup.command, &mut store)).expect(
+                    "erooorrrr"
+                    );
+            
+
+                
+
+            
+
         } else if tup.ty == Some("get".to_string()) {
            write!(stream, "{}", get_set_cahcer(tup.ty.to_owned().unwrap(), &tup.command, &mut store)).expect(
                 "erooorrrr"
@@ -82,21 +88,37 @@ fn responder(stream: &mut TcpStream, mut  store: Arc<Mutex<HashMap<String,String
     }
 }
 
-fn get_set_cahcer(method: String, commands: &Vec<Option<String>>,store:&mut Arc<Mutex<HashMap<String,String>>>) -> String {
+fn get_set_cahcer(method: String, commands: &Vec<Option<String>>,store:&mut Arc<Mutex<HashMap<String,(String,u64)>>>) -> String {
 
     if method == "set" {
-        let key = &commands[0].to_owned().unwrap();
+        if commands.len() == 3{
+
+ let key = &commands[0].to_owned().unwrap();
         let value = &commands[1].to_owned().unwrap();
 
-        let res = store.lock().unwrap().insert(key.to_string(), value.to_string());
+        let res = store.lock().unwrap().insert(key.to_string(), (value.to_string(),0));
 
         // println!("{:?}",store);
         if res == None {
             return simple_string_encoder(&"OK".to_string());
         } else {
-            return simple_string_encoder(&res.unwrap().to_string());
+            return simple_string_encoder(&res.unwrap().0.to_string());
         }
-    } else {
+        }
+        else  {
+            let mut since_the_epoch = SystemTime::now().duration_since(UNIX_EPOCH).unwrap() ;
+            let x = since_the_epoch.as_secs() * 1000 +
+            since_the_epoch.subsec_nanos() as u64 / 1_000_000+
+             commands[3].to_owned().unwrap().parse::<u64>().unwrap();
+             let val  = commands[1].to_owned().unwrap()  ;
+             let res =  store.lock().unwrap().insert(commands[0].to_owned().unwrap(), (val,x));
+             println!("{:?}",store.lock().unwrap());
+                 
+            return  simple_string_encoder(&"OK".to_string());
+
+        }
+        }
+        else {
         let key = &commands[0].to_owned().unwrap();
         let res = store.lock().unwrap();
         let res = res.get(key);
@@ -104,7 +126,7 @@ fn get_set_cahcer(method: String, commands: &Vec<Option<String>>,store:&mut Arc<
         if res == None {
             return simple_string_encoder(&"nil".to_string());
         } else {
-            return simple_string_encoder(&res.unwrap().to_string());
+            return simple_string_encoder(&res.unwrap().0.to_string());
         }
     }
 }
